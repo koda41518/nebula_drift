591a1c644bcff4f89d3421f40f98c7d89fe99f41
koda41518 <155271337+koda41518@users.noreply.github.com>
2025-08-16 07:03:35 +0100

    u

--- entities_classes/__init__.py
+++ entities_classes/__init__.py
@@ -5,7 +5,7 @@ from .enemies import Enemy
 from .repair import Repair
 from .camera import Camera
 from .background import StarField
-from .screen_effects import ScreenEffects
+from .screen_effects import DamageFlash
 
 
 
